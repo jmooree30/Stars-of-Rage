d7b51948c210902593ed4322cfab4d2b2e933af1
Jacob <jmooree30@gmail.com>
2018-01-20 23:53:01 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Stars-of-Rage
+
+![Image Hover Text](/ss.png)
+
+Still in development! =)
+
+### Installation for Ubuntu/Mint:
+```
+# Dependencies for both C++ and Ruby
+sudo apt-get install build-essential libsdl2-dev libsdl2-ttf-dev libpango1.0-dev \
+                     libgl1-mesa-dev libopenal-dev libsndfile-dev libmpg123-dev \
+                     libgmp-dev
+
+# To install Ruby itself - if you are using rvm or rbenv, please skip this step
+sudo apt-get install ruby-dev
+
+# If you are using a Ruby version manager (i.e. rvm or rbenv)
+gem install gosu
+# If you are using system Ruby, you will need "sudo" to install Ruby libraries (gems)
+sudo gem install gosu
+```
+Clone the repo:<br>
+`git clone https://github.com/jmooree30/Stars-of-Rage.git`<br>
+Start game:<br>
+`ruby main.rb`
+
+Different OS? Check out the Gosu [getting started](https://github.com/gosu/gosu/wiki/getting-started-on-linux) page.<br>
+
+### Features
+* Scrolling background: &#10004;
+* Music: &#10004;
+* Control player movement: &#10004;
+* Intro screen: &#10004;
+* Boundary collision: &#10004;
+* enemies: &#65794;
+* shooting: &#65794;
+* Lives: &#65794;
+* scores/high scores: &#65794;
--- main.rb
+++ main.rb
@@ -9,68 +9,77 @@ class GameWindow < Gosu::Window
     @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
     @soundtrack = Gosu::Song.new(self,'sor.mp3')
     @wallhit = Gosu::Sample.new(self,'blip.wav')
-    @soundtrack.play(true)
     @player = Gosu::Image.new(self,'starfighter.bmp', false)
-    @enemy = Gosu::Image.new(self,'starfighter.bmp', false)
     @background = Gosu::Image.new(self,'max.jpg')
     @bgrepeat = Gosu::Image.new(self,'max.jpg')
     @x = 320 - @player.width/2
     @y = 340 - @player.height/2
-    @a = 0
     @b = -@background.height + 480
-    @c = 0
     @d = -@background.height * 2 + 480
+    @true = false
   end
   
   def update
-
-    if @d > 480
-       @d = -@background.height
-    end
-
-    if @b > 480
-      @b = -@background.height
-    end    
-    @b +=2
-    @d +=2
-    collision
+   if @d > 480
+     @d = -@background.height
+   end
+   if @b > 480
+    @b = -@background.height
+   end    
+  @b +=2
+  @d +=2
+  collision
   end
 
-  def collision
-    if @x < 0
-      @x +=1
-      @wallhit.play
-    elsif @x > (640 - @player.width)
-      @x -=1
-      @wallhit.play
-    elsif @y < 0
-      @y +=1
-      @wallhit.play
-    elsif @y > (480 - @player.height)
-      @y -=1
-      @wallhit.play
-    else 
-      @x -= 4 if button_down?(Gosu::KbLeft)
-      @x += 4 if button_down?(Gosu::KbRight)
-      @y -= 4 if button_down?(Gosu::KbUp)
-      @y += 4 if button_down?(Gosu::KbDown)
-    end
+def collision
+  if @x < 0
+    @x +=1
+    @wallhit.play
+  elsif @x > (640 - @player.width)
+    @x -=1
+    @wallhit.play
+  elsif @y < 0
+    @y +=1
+    @wallhit.play
+  elsif @y > (480 - @player.height)
+    @y -=1
+    @wallhit.play
+  else 
+    @x -= 4 if button_down?(Gosu::KbLeft)
+    @x += 4 if button_down?(Gosu::KbRight)
+    @y -= 4 if button_down?(Gosu::KbUp)
+    @y += 4 if button_down?(Gosu::KbDown)
   end
+end
 
 
-  def draw
-    @font.draw(@b,0,0,4)
-    @player.draw(@x, @y,1)
-    @background.draw(@a,@b,0)
-    @bgrepeat.draw(@c,@d,0)
-    @enemy.draw(@a,@b,1)
+def draw
+  start_game
+end
+
+def start_game
+  @font.draw("Welcome to Stars of Rage!",210,140,0)
+  @font.draw("Press space to start",230,160,0)
+  if button_down?(Gosu::KbSpace)
+    @true = true 
   end
-  
-  def button_down(id)
-    if id == Gosu::KbEscape
-      close
-    end
+  if @true == true
+    main
   end
+end
+
+def main
+  @player.draw(@x, @y,1)
+  @background.draw(0,@b,0)
+  @bgrepeat.draw(0,@d,0)
+  @soundtrack.play(true)
+end
+
+def button_down(id)
+  if id == Gosu::KbEscape
+    close
+  end
+end
 
 end
 
