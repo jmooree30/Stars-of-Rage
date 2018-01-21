a737e32579a26250c9fc7630309c37909b74b4b2
Jacob <jmooree30@gmail.com>
2018-01-20 21:45:34 -0500

    Moving background

--- main.rb
+++ main.rb
@@ -6,20 +6,33 @@ class GameWindow < Gosu::Window
   def initialize
     super(640,480,false)
     self.caption = "Stars of Rage"
+    @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
     @soundtrack = Gosu::Song.new(self,'sor.mp3')
     @wallhit = Gosu::Sample.new(self,'blip.wav')
     @soundtrack.play(true)
     @player = Gosu::Image.new(self,'starfighter.bmp', false)
     @enemy = Gosu::Image.new(self,'starfighter.bmp', false)
-    @background = Gosu::Image.new(self,'background.jpg')
+    @background = Gosu::Image.new(self,'max.jpg')
+    @bgrepeat = Gosu::Image.new(self,'max.jpg')
     @x = 320 - @player.width/2
     @y = 340 - @player.height/2
     @a = 0
-    @b = 0
+    @b = -@background.height + 480
+    @c = 0
+    @d = -@background.height * 2 + 480
   end
   
   def update
-    @b +=3
+
+    if @d > 480
+       @d = -@background.height
+    end
+
+    if @b > 480
+      @b = -@background.height
+    end    
+    @b +=2
+    @d +=2
     collision
   end
 
@@ -46,8 +59,10 @@ class GameWindow < Gosu::Window
 
 
   def draw
+    @font.draw(@b,0,0,4)
     @player.draw(@x, @y,1)
-    @background.draw(0,0,0)
+    @background.draw(@a,@b,0)
+    @bgrepeat.draw(@c,@d,0)
     @enemy.draw(@a,@b,1)
   end
   
